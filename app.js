c23bfc319619aa14d8690d0c3563a89c505d9e03
Connor Ketcheson <connorketcheson01@gmail.com>
2023-09-14 09:22:04 +0900

    Added files and changes required for deployment

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(limiter);
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
 
-const mongoDB = process.env.MONGODB_URI;
+const mongoDB = process.env.MONGO_URL;
 
 main().catch((err) => console.log(err));
 async function main() {
